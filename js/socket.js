85f8fff68e25d653660f643d44ded026e6d8cce9
Aboobacker MK <aboobackervyd@gmail.com>
2014-10-24 12:01:04 +0530

    implemented group chat

--- js/socket.js
+++ js/socket.js
@@ -1,31 +1,69 @@
 $(document).ready(function(){
-    //Open a WebSocket connection.
-    var wsUri = "ws://localhost:8080";   
-    websocket = new WebSocket(wsUri); 
-    
-    //Connected to server
-    websocket.onopen = function(ev) {
-        alert('Connected to server ');
-    }
-    
-    //Connection close
-    websocket.onclose = function(ev) { 
-        alert('Disconnected');
-    };
-    
-    //Message Receved
-    websocket.onmessage = function(ev) { 
-        alert('Message '+ev.data);
-    };
-    
-    //Error
-    websocket.onerror = function(ev) { 
-        alert('Error '+ev.data);
-    };
-    
-     //Send a Message
-    $('#send').click(function(){ 
-        var mymessage = 'This is a test message'; 
-        websocket.send(mymessage);
+    $('#username_modal').modal('show');
+    var reciever="all"
+    $('#continue_button').click(function() {
+        var username=$('#inputUsername').val();
+        $('#username_modal').modal('toggle');
+        alert(username);
+        //Open a WebSocket connection.
+        var wsUri = "ws://192.168.1.211:8080/"+username;   
+        websocket = new WebSocket(wsUri); 
+        //Connected to server
+        websocket.onopen = function(ev) {
+            alert('Connected to server ');
+        }
+        
+        //Connection close
+        websocket.onclose = function(ev) { 
+            alert('Disconnected');
+        };
+        
+        //Message Receved
+        websocket.onmessage = function(ev) {
+            var json =  jQuery.parseJSON(ev.data);
+            var time,cur_time;
+            time=new Date($.now());
+            cur_time=time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
+            var message="<div class=\"message well\"><p>"+json.message+"</p> <p class=\"pull-right\">"+json.user+"</p>    <li><span><i class=\"glyphicon glyphicon-calendar\"></i> "+cur_time+" </span></li></div>";
+            $('.chat_area').append(message);
+            //alert(message);
+            $('#connected_users').text("")
+            $('#sendto')
+                .empty()
+                //.append('<option selected="selected" value="all">All</option>')
+            ;
+            for (index = 0; index < json.users.length; ++index) {
+                $('#connected_users').append("<li>"+json.users[index]+"</li>");
+                $("#sendto").append('<option value='+json.users[index]+'>'+json.users[index]+'</option>');
+                $('#sendto').multiSelect('refresh');                
+            }
+            $('#sendto').multiSelect()
+            //alert('Message '+ev.data);
+        };
+        
+        //Error
+        websocket.onerror = function(ev) { 
+            alert('Error '+ev.data);
+        };
+        
+         //Send a Message
+        $('#send').click(function(){ 
+            var mymessage = $('#post_input').val();
+            $('#post_input').val('');
+            //websocket.send(JSON.stringify('user_message', {name: 'UserName', message: mymessage}) );
+            var msg=JSON.stringify({ message: mymessage,user: username ,to:reciever}); 
+            websocket.send(msg);
+        });
+        $( "#sendto" ).change(function() {
+          reciever=$( "#sendto" ).val();
+          //alert(reciever);
+        });
+        $('#create_chat_button').click(function(){
+            var selected_users=$( "#sendto" ).val();
+            alert(selected_users);
+            $('#myModal').modal('hide');
+        });
+
+        //$('#sendto').multiSelect()
     });
 });
--- server.rb
+++ server.rb
@@ -1,22 +1,54 @@
 require 'em-websocket'
+require 'eventmachine'
+require 'json'
 
 EM.run {
-  EM::WebSocket.run(:host => "0.0.0.0", :port => 8080) do |ws|
+    @clients = []
+    @user_list= []
+  EM::WebSocket.run(:host => "0.0.0.0", :port => 8080 ,:debug => true) do |ws|
     ws.onopen { |handshake|
+      @clients << ws
+      @user_list << handshake.path.sub!('/', '')
       puts "WebSocket connection open"
-
-      # Access properties on the EM::WebSocket::Handshake object, e.g.
-      # path, query_string, origin, headers
-
-      # Publish message to the client
-      ws.send "Hello Client, you connected to #{handshake.path}"
+      json={:message => "Hello Client, you connected",:user=>"admin",:users=>@user_list}.to_json
+      ws.send json
+      #ws.send "Hello Client, you connected"
+      @clients.each do |socket|
+        json={:message => " #{handshake.path} Logged In",:user=>"admin",:users=>@user_list}.to_json
+        socket.send json
+        #socket.send " #{handshake.path} Logged In" 
+      end
     }
 
-    ws.onclose { puts "Connection closed" }
+    ws.onclose { 
+      puts "Connection closed"
+      @user_list.delete_at( @clients.index(ws))
+      @clients.delete ws
+      @clients.each do |socket|
+        socket.send "A user logged out"
+      end
+    }
 
     ws.onmessage { |msg|
       puts "Recieved message: #{msg}"
-      ws.send "Pong: #{msg}"
+      obj = JSON.parse(msg)
+      if obj['to']=="all"
+        @clients.each do |socket|
+          json={:type=>"message",:message => obj['message'],:user=>obj['user'],:users=>@user_list}.to_json
+          socket.send json
+          #socket.send msg
+        end
+      else
+        obj['to'].each do |to_obj|
+          socket=@clients[@user_list.index(to_obj)]
+          json={:type=>"message",:message => obj['message'],:user=>obj['user'],:users=>@user_list}.to_json
+          socket.send json
+        end
+      end
+        #ws.send "#{msg}"
+    }
+    ws.onerror {
+      puts 'ERRORRRRR'
     }
   end
 }
